03daa99b7242a240e141f13b3899e5911eb09779
fry <fry.land@hotmail.fr>
2023-02-20 07:05:04 +0100

    adding GetUser Decorator + auth guards for userController

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, UseGuards, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guards';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('/profile')
+  getProfile(@GetUser() user: User) {
+    return user;
+  }
+}
