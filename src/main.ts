1b94a25df8b919c95a1e1a31a25029042684ae1d
fry <fry.land@hotmail.fr>
2023-03-09 13:04:42 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(cors());
 
   await app.listen(5500);
 }
 bootstrap();
+function cors(): any {
+  throw new Error('Function not implemented.');
+}
