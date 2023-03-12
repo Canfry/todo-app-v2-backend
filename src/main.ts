6943654accedd37d6e04131c9d6f9a67b0f54a2f
fry <fry.land@hotmail.fr>
2023-03-12 08:51:10 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  });
+  app.enableCors();
   await app.listen(5500);
 }
 bootstrap();
