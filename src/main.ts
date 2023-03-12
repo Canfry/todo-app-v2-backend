c76ce5cf52a15b7c434f5298c2ce8919379abdad
fry <fry.land@hotmail.fr>
2023-03-12 08:36:59 +0100

    adding vercel.json

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(5500);
 }
 bootstrap();
