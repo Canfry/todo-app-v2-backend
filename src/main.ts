72e250df3f7cbcd2384d5643fda4899f396916a4
fry <fry.land@hotmail.fr>
2023-03-12 09:20:09 +0100

    trying again cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    preflightContinue: false,
     origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    optionsSuccessStatus: 204,
   });
   await app.listen(5500);
 }
