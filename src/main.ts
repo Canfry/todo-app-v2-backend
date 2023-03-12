c08bcb32a8eb0c3764aa8ea3d3155f35ff404d84
fry <fry.land@hotmail.fr>
2023-03-12 09:55:28 +0100

    adding methods

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
   await app.listen(5500);
 }
