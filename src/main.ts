4e69fc4b1c041a24da68c6f1edb70cfd932a7586
fry <fry.land@hotmail.fr>
2023-03-23 10:16:36 +0100

    adding PORT env

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  });
-  await app.listen(5500);
+  app.enableCors();
+  await app.listen(process.env.PORT || 5500);
 }
 bootstrap();
