8a80affc61e0a65eb863e109e23147ff451ed141
fry <fry.land@hotmail.fr>
2023-03-23 17:53:57 +0100

    adding origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT || 5500);
 }
 bootstrap();
