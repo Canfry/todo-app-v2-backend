f9c50eb80853059d57e30f94e9cb336a47830784
fry <fry.land@hotmail.fr>
2023-03-23 15:24:24 +0100

    change cors options

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:5173',
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 5500);
 }
 bootstrap();
+
+// allowedHeaders: ['content-type'],
+//     origin: 'http://localhost:5173',
