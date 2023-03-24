83cbc15c16e18f1d15047903df484fd20faeb5b8
fry <fry.land@hotmail.fr>
2023-03-24 10:08:39 +0100

    changing app.listen

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://todo-app-v2-frontend.vercel.app',
   });
-  await app.listen(process.env.PORT || 5500);
+  await app.listen(5500);
 }
 bootstrap();
 
