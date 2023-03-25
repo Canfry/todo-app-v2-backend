1e626f77cfa3beeaee8733917a0f9f377459fe90
fry <fry.land@hotmail.fr>
2023-03-25 08:29:22 +0100

    removing origin

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://www.todo-app-v2-frontend.vercel.app',
-  });
+  app.enableCors();
   await app.listen(5500);
 }
 bootstrap();
 
 // allowedHeaders: ['content-type'],
 //     origin: 'http://localhost:5173',
+
+// {
+//     origin: 'https://www.todo-app-v2-frontend.vercel.app',
+//   }
