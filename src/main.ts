929724659d4be023e463193eb148a634398463ea
fry <fry.land@hotmail.fr>
2023-04-02 07:22:41 +0200

    changing origon

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://www.todo-app-v2-frontend.vercel.app',
+    origin: 'https://todo-app-v2-frontend.vercel.app',
   });
   await app.listen(5500);
 }
