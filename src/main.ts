d3db637815aeca0a436271456b10698c6b7b9f62
fry <fry.land@hotmail.fr>
2023-04-03 18:05:55 +0200

    removing origin

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-  });
+  app.enableCors();
   await app.listen(5500);
 }
 bootstrap();
